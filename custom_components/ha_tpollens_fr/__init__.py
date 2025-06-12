ea9f7251b9e4f1872d8881e405f77ae87d375502
trollix <dr.chandra@laposte.net>
2025-06-12 13:23:47 +0200

    release v0.1.18

--- custom_components/ha_tpollens_fr/__init__.py
+++ custom_components/ha_tpollens_fr/__init__.py
@@ -12,10 +12,12 @@ async def async_setup(hass: HomeAssistant, config: ConfigType):
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     coordinator = PollensCoordinator(hass, entry)
     await coordinator.async_config_entry_first_refresh()
+
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
 
+    # Assure-toi que la plateforme est bien "sensor"
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setups(entry, "sensor")
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
     )
     return True
 
