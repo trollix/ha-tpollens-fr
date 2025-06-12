0fda494b8ad2b5b501354483b524fb170e83e179
trollix <dr.chandra@laposte.net>
2025-06-12 12:36:30 +0200

    release v0.1.16

--- custom_components/ha_tpollens_fr/__init__.py
+++ custom_components/ha_tpollens_fr/__init__.py
@@ -15,7 +15,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
 
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+        hass.config_entries.async_forward_entry_setups(entry, "sensor")
     )
     return True
 
